7833abea8ad7d366545d424d2a444f3c4a50937e
Mark Phillips <markphillips@Marks-MacBook-Pro.local>
2020-08-20 13:56:04 +0100

    Remove auto generated files

--- packages/app/src/index.js
+++
@@ -1,9 +0,0 @@
-import { InputText } from '@form/components';
-import React from 'react';
-import { render } from 'react-dom';
-import { GlobalStyle } from './styles';
-var App = function () { return (React.createElement(React.Fragment, null,
-    React.createElement(GlobalStyle, null),
-    React.createElement(InputText, { label: "First Name", name: "firstName" }))); };
-var rootElement = document.getElementById('root');
-render(React.createElement(App, null), rootElement);
--- packages/app/src/styles.js
+++
@@ -1,8 +0,0 @@
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
-import { createGlobalStyle } from 'styled-components';
-var fonts = { size: '14px', family: 'sans-serif' };
-export var GlobalStyle = createGlobalStyle(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  * {\n    box-sizing: border-box;\n  }\n  body, h1, h2 {\n    margin: 0;\n  }\n  html, body {\n    height: 100%;\n    width: 100%;\n  }\n  body {\n      font-family: ", ";\n      padding: 0;\n      font-weight: normal;\n      font-size:", ";\n  }\n  #root {\n    height: 100%;\n    width: 100%;\n  }\n"], ["\n  * {\n    box-sizing: border-box;\n  }\n  body, h1, h2 {\n    margin: 0;\n  }\n  html, body {\n    height: 100%;\n    width: 100%;\n  }\n  body {\n      font-family: ", ";\n      padding: 0;\n      font-weight: normal;\n      font-size:", ";\n  }\n  #root {\n    height: 100%;\n    width: 100%;\n  }\n"])), fonts.family, fonts.size);
-var templateObject_1;
